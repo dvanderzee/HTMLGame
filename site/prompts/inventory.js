9453f600f37c2d285f414c46d3952441b0487eb5
ninjadinosir <ninjadinosir@gmail.com>
2015-04-07 16:36:38 -0400

    the inventory and stats script,  it currently requires a "tester" and "tester2" div id to operate

---
+++ site/prompts/inventory.js
@@ -0,0 +1,71 @@
+
+
+//stores all of the characters current inventory,starts with a few things
+var inventory={
+	"Leather Plate":{type:"Armor",count:1},
+	"Dagger":{type:"Weapon",count:1},
+	"Leather Cap":{type:"Helmet",count:1},
+	"Gold":{type:"Currency",count:100}
+};
+
+//stores all of the current character's equipped items
+var equipment={
+	Head:inventory["Leather Cap"],
+	Body:"empty",
+	Weapon:"empty",
+	Ring1:"empty",
+	Ring2:"empty",
+	Legs:"empty"
+};
+//creates Equipment header
+var newPart=document.createElement("h2");
+newPart.appendChild(document.createTextNode("Equipment"));
+var area=document.getElementById("tester2");
+area.appendChild(newPart);
+//steps through the equipment array and "prints" out each piece of equipment
+for (var key in equipment){
+	var newPart=document.createElement("p");
+	newPart.appendChild(document.createTextNode(key));
+	newPart.appendChild(document.createTextNode(": "));
+	newPart.appendChild(document.createTextNode(equipment[key]));
+	var area=document.getElementById("tester2");
+	area.appendChild(newPart);
+}
+//creates Inventory heeader
+var newPart=document.createElement("h2");
+newPart.appendChild(document.createTextNode("Inventory"));
+var area=document.getElementById("tester2");
+area.appendChild(newPart);
+//steps through each item in inventory array and "prints" them
+for (var key in inventory){
+	var newPart=document.createElement("p");
+	newPart.appendChild(document.createTextNode(key));
+	newPart.appendChild(document.createTextNode(": "));
+	temp=inventory[key];
+	newPart.appendChild(document.createTextNode(temp.count));
+	var area=document.getElementById("tester2");
+	area.appendChild(newPart);
+}
+
+//will add an item to the equipment if the associated area is open
+function addequip(){
+	
+}
+//removes the current item from the equipment area, sets that area back to "empty"
+function removeequip(){
+	
+}
+//will add objects to the inventory,if they're already present, add to count
+function addinventory(){
+	
+}
+//will remove objects from the inventory,if count of that object is 0, remove it from array
+//unless it is "gold" in which case it stays as zero
+function removeinventory(){
+	
+}
+//will eventually handle printing and will be called each time something changes
+function printinventory(){
+	
+}
+
---
+++ site/prompts/stats.js
@@ -0,0 +1,98 @@
+
+//will hold our base stats, unmodified at all
+var BaseStats={
+	Level:1,
+	Strength:10,
+	Toughness:10,
+	Dexterity:10,
+	Intelligence:10,
+	Willpower:10,
+	Fellowship:10,
+	MaxHP:100,
+	MaxMP:50
+}
+
+//will hold temporary stats, modified by damage dealt recently, possibly equipment,
+//and other stats(since HP and MP are effected by other stats)
+var ModifiedStats={
+	Level:BaseStats.Level,
+	Strength:BaseStats.Strength,
+	Toughness:BaseStats.Toughness,
+	Dexterity:BaseStats.Dexterity,
+	Intelligence:BaseStats.Intelligence,
+	Willpower:BaseStats.Willpower,
+	Fellowship:BaseStats.Fellowship,
+	MaxHP:BaseStats.MaxHP+10*(BaseStats.Toughness-10),
+	MaxMP:BaseStats.MaxMP+5*(BaseStats.Intelligence-10),
+	CurrentHP:BaseStats.MaxHP+10*(BaseStats.Toughness-10),
+	CurrentMP:BaseStats.MaxMP+5*(BaseStats.Intelligence-10),
+	XPtoNextLevel:25*BaseStats.Level,
+	XP:0
+}
+
+ModifiedStats.CurrentHP-=10;
+
+var newPart=document.createElement("h2");
+newPart.appendChild(document.createTextNode("Stats:"));
+var area=document.getElementById("tester");
+area.appendChild(newPart);
+
+for (var key in ModifiedStats){
+	var newPart=document.createElement("p");
+	newPart.appendChild(document.createTextNode(key));
+	newPart.appendChild(document.createTextNode(": "));
+	newPart.appendChild(document.createTextNode(ModifiedStats[key]));
+	var area=document.getElementById("tester");
+	area.appendChild(newPart);
+}
+
+LevelUp("Intelligence","Willpower")
+
+var newPart=document.createElement("h2");
+newPart.appendChild(document.createTextNode("Updated Stats:"));
+var area=document.getElementById("tester");
+area.appendChild(newPart);
+
+for (var key in ModifiedStats){
+	var newPart=document.createElement("p");
+	newPart.appendChild(document.createTextNode(key));
+	newPart.appendChild(document.createTextNode(": "));
+	newPart.appendChild(document.createTextNode(ModifiedStats[key]));
+	var area=document.getElementById("tester");
+	area.appendChild(newPart);
+}
+
+//function for levelling up. It assumes we only allow to stats to be increased per level
+//Also fully heals the character since that's fairly common
+function LevelUp(x,y){
+	BaseStats.Level+=1;
+	BaseStats.MaxHP+=10;
+	BaseStats.MaxMP+=5;
+	BaseStats[x]+=5;
+	BaseStats[y]+=5;
+	ModifiedStats.Level=BaseStats.Level;
+	ModifiedStats.MaxHP=BaseStats.MaxHP+10*(BaseStats.Toughness-10);
+	ModifiedStats.MaxMP=BaseStats.MaxMP+5*(BaseStats.Intelligence-10);
+	ModifiedStats.CurrentHP=ModifiedStats.MaxHP;
+	ModifiedStats.CurrentMP=ModifiedStats.MaxMP;
+	ModifiedStats[x]=BaseStats[x];
+	ModifiedStats[y]=BaseStats[y];
+	ModifiedStats.XPtoNextLevel*=4;
+	ModifiedStats.XP=0;
+}
+
+//function for updating modified stats depending upon buffs/debuffs from equipment and monsters
+//currently just resets all stats to their base forms
+function updateModifiedStats(){
+	ModifiedStats.Strength=BaseStats.Strength;
+	ModifiedStats.Toughness=BaseStats.Toughness;
+	ModifiedStats.Dexterity=BaseStats.Dexterity;
+	ModifiedStats.Intelligence=BaseStats.Intelligence;
+	ModifiedStats.Willpower=BaseStats.Willpower;
+	ModifiedStats.Fellowship=BaseStats.Fellowship;
+	ModifiedStats.MaxHP=BaseStats.MaxHP+10*BaseStats.Toughness;
+	ModifiedStats.MaxMP=BaseStats.MaxMP+5*BaseStats.Intelligence;
+}
+
+
+
