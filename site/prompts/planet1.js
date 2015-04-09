e2e20693c6423f774261d9e8532bcc92d34df96a
Miles Konstantin <mileskonstantin@gmail.com>
2015-04-09 01:30:54 -0400

    Created a blank template planet and added support for item, game, and new planet triggers.

--- site/prompts/planet1.js
+++ site/prompts/planet1.js
@@ -4,32 +4,94 @@ var levels = {};
 //The letters after the question number indicate the history of choices to get to that point in the branch
 
 levels._1_ = {
-	prompt: "An alien approaches and greets you.",
-	a: "Wave",
-	b: "Kill it"
-	//location: "" [Info about the current area could go here if we are doing nav through areas]
+	prompt: "Prompt 1",
+	a: "Option A",
+	b: "Option B"
 }
 
 levels._2_a = {
-	prompt: "The alien befriends you and gives you a mountain of treasure.",
-	a: "Retire",
-	b: "Sell and buy a ship"
+	prompt: "Prompt 2_a",
+	a: "Option A",
+	b: "Option B"
 }
 
 levels._2_b = {
-	prompt: "The dead alien's blood is all over you. Guards have been alerted.",
-	a: "Hide",
-	b: "Get ready to fight"
+	prompt: "Prompt 2_b",
+	a: "Option A",
+	b: "Option B"
 }
 
 levels._3_aa = {
-	prompt: "You fly home and start a family.",
-	a: "Game over",
-	b: "Play again"
+	prompt: "Prompt 3_aa",
+	a: "Option A",
+	b: "Option B",
+	item: "item1"
 }
 
 levels._3_ab = {
-	prompt: "You use warp drive to visit the next planet.",
-	a: "Land in the city",
-	b: "Land in the wilderness"
+	prompt: "Prompt 3_ab",
+	a: "Option A",
+	b: "Option B"
+}
+
+levels._3_ba = {
+	prompt: "Prompt 3_ba",
+	a: "Option A",
+	b: "Option B"
+}
+
+levels._3_bb = {
+	prompt: "Prompt 3_bb",
+	a: "Option A",
+	b: "Option B"
+}
+
+levels._4_aaa = {
+	prompt: "Prompt 4_aaa",
+	a: "Option A",
+	b: "Option B",
+	game: true
+}
+
+levels._4_aab = {
+	prompt: "Prompt 4_aab",
+	a: "Option A",
+	b: "Option B",
+	newplanet: true
+}
+
+levels._4_aba = {
+	prompt: "Prompt 4_aba",
+	a: "Option A",
+	b: "Option B"
+}
+
+levels._4_abb = {
+	prompt: "Prompt 4_abb",
+	a: "Option A",
+	b: "Option B"
+}
+
+levels._4_bba = {
+	prompt: "Prompt 4_bba",
+	a: "Option A",
+	b: "Option B"
+}
+
+levels._4_bbb = {
+	prompt: "Prompt 4_bbb",
+	a: "Option A",
+	b: "Option B"
+}
+
+levels._4_baa = {
+	prompt: "Prompt 4_baa",
+	a: "Option A",
+	b: "Option B"
+}
+
+levels._4_bab = {
+	prompt: "Prompt 4_bab",
+	a: "Option A",
+	b: "Option B"
 }
