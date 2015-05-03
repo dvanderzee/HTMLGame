dee498d31c2822b0039dff045acf85f56100efbd
ninjadinosir <ninjadinosir@gmail.com>
2015-05-03 02:26:42 -0400

    better setup for everything, found out why the example game wasn't operating right, fixed it sorta, next making it implement our stuff

--- site/prompts/stats.js
+++ site/prompts/stats.js
@@ -1,7 +1,7 @@
 
 
 //initializes our base stats,all functions can access it
-var BaseStats={
+var Stats={
 	Level:1,
 	Dexterity:10,
 	Fellowship:10,
@@ -26,15 +26,15 @@ function statsmain(){
 	area.appendChild(newPart);
 	var mytable=document.createElement("table");
 
-	//steps through BaseStats object and creates each stat display
-	for (var key in BaseStats){
+	//steps through Stats object and creates each stat display
+	for (var key in Stats){
 		var row=mytable.insertRow();
 		var cellname=row.insertCell();
 		var cellvalue=row.insertCell();
 		cellname.style.width="100px";
 		cellvalue.style.textAlign="right";
 		cellname.innerHTML=key;
-		cellvalue.innerHTML=BaseStats[key];
+		cellvalue.innerHTML=Stats[key];
 		cellvalue.id="stats"+key;
 	}
 	area.appendChild(mytable);
@@ -45,41 +45,45 @@ function statsmain(){
 //function for levelling up. It assumes we only allow to stats to be increased per level
 //Also fully heals the character since that's fairly common
 function LevelUp(x,y){
-	BaseStats.Level+=1;
-	BaseStats[x]+=5;
-	BaseStats[y]+=5;
-	BaseStats.MaxHP=10*BaseStats.Toughness+10*(BaseStats.Level-1);
-	BaseStats.HP=BaseStats.MaxHP;
-	BaseStats.MaxMP=5*BaseStats.Intelligence+5*(BaseStats.Level-1);
-	BaseStats.MP=BaseStats.MaxMP;
-	BaseStats.XP-=BaseStats.XPtoLevel;
-	BaseStats.XPtoLevel*=4;
+	Stats.Level+=1;
+	Stats[x]+=5;
+	Stats[y]+=5;
+	Stats.MaxHP=10*Stats.Toughness+10*(Stats.Level-1);
+	Stats.HP=Stats.MaxHP;
+	Stats.MaxMP=5*Stats.Intelligence+5*(Stats.Level-1);
+	Stats.MP=Stats.MaxMP;
+	Stats.XP-=Stats.XPtoLevel;
+	Stats.XPtoLevel*=4;
 	UpdateDisplay();
 }
 
 //will update the html display
 function UpdateDisplay(){
-	for (var key in BaseStats){
+	for (var key in Stats){
 		var area=document.getElementById("stats"+key);
-		area.innerHTML=BaseStats[key];
+		area.innerHTML=Stats[key];
 	}
 }
 
 //if only 1 stat is being increased (mostly XP)
 function PlusStat(stat,amount){
-	BaseStats[stat]+=amount;
-	if (stat="XP" && BaseStats.XPtoLevel<=BaseStats.XP){
+	Stats[stat]+=amount;
+	if (stat="XP" && Stats.XPtoLevel<=Stats.XP){
 		//change main display to ask what stats to level up and then run levelup
 		//with those 2 stats
 		LevelUp("Toughness","Intelligence");
+	}else if(stat='HP' && Stats.MaxHP<=Stats.HP){
+		Stats.HP=Stats.MaxHP;
+	}else if(stat='MP' && Stats.MaxMP<=Stats.MP){
+		Stats.MP=Stats.MaxMP;
 	}
 	UpdateDisplay();
 }
 
 //if a stat is being reduced (mostly applicable to health and MP)
 function MinusStat(stat,amount){
-	BaseStats[stat]-=amount;
-	if (stat="HP" && BaseStats.HP<0){
+	Stats[stat]-=amount;
+	if (stat="HP" && Stats.HP<0){
 		//print game over,restart game
 		var area = document.getElementById("statMenu");
 		area.innerHTML="YOU DIED!";
